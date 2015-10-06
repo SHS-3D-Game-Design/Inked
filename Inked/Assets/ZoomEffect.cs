8074a2ea266319da7a030d34d7f843e45a73a835
FCL00061 <frizzell.tom@gmail.com>
2015-10-06 17:14:46 -0600

    Recent Changes along with most Photon Networking

--- Inked/Assets/ZoomEffect.cs
+++ Inked/Assets/ZoomEffect.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class ZoomEffect : MonoBehaviour {
 	public GameObject zoomview;
 
+
 	void Start (){
 		zoomview.SetActive(false);
 		}
 	// Update is called once per frame
 	void Update () {
-if (Input.GetMouseButtonDown (1)) 
-		{zoomview.SetActive(true);
+if (Input.GetMouseButton (1)) 
+		{
+			zoomview.SetActive(true);
 		}
 		else{zoomview.SetActive(false);
 						}
-				}
+	}
 }
 
